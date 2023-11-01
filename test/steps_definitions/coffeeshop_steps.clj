2ccf871b46af11148629944d5743663fe36f3a2a
Mpumi Mathenjwa <mjmathenjwa@gmail.com>
2023-11-01 11:07:27 +0200

    added project to repo

---
+++ test/steps_definitions/coffeeshop_steps.clj
@@ -0,0 +1,28 @@
+(ns coffeeshop-steps
+  (:require [lambdaisland.cucumber.dsl :refer :all]
+            [clojure.test :refer :all]))
+
+(Given "the following price list" [state table]
+       (assoc state
+              :price-list
+              (into {}
+                    (map (fn [[k v]]
+                           [k (Double/parseDouble v)]))
+                    table)))
+(When "I order a (.*)" [state product]
+      (update state :order conj product))
+
+(And "pay with ${double}" [{:keys [price-list order] :as state} paid]
+     (doseq [product order]
+       (is (contains? price-list product)))
+     (let [total (get-in price-list order)]
+       (is (<= total paid))
+       (assoc state
+              :total total
+              :paid  paid
+              :change (- paid total))))
+
+(Then "I get ${double} back" [{:keys [change] :as state} expected]
+      (is (= expected change))
+      state)
+
